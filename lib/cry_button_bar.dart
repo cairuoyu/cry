ad417ea02460f2ac5f6181d16164b7e4d2901175
cairuoyu <cry_cairuoyu@163.com>
2021-04-06 10:19:22 +0800

    ## 1.0.3 * 新增CryButtonBar

---
+++ lib/cry_button_bar.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CryButtonBar extends Wrap {
+  CryButtonBar({
+    List<Widget> children = const <Widget>[],
+  }) : super(children: children, spacing: 5.0, runSpacing: 5.0);
+}
